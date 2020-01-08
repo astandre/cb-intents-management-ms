47ebfd30a3e2fe22b392b3518273b190f946b713
Andre <andreherrera97@hotmail.com>
2020-01-07 21:08:27 -0500

    Changes for deploy in docker

--- setup.py
+++ setup.py
@@ -2,16 +2,19 @@ import sys
 from setuptools import setup, find_packages
 from kbsbot.intents_managment import __version__
 
-
 with open('requirements.txt') as f:
     deps = [dep for dep in f.read().split('\n') if dep.strip() != ''
             and not dep.startswith('-e')]
     install_requires = deps
-
 
 setup(name='intents_managment',
       version=__version__,
       packages=find_packages(),
       zip_safe=False,
       include_package_data=True,
-      install_requires=install_requires)
+      install_requires=install_requires,
+      entry_points="""
+      [console_scripts]
+      intents_managment = kbsbot.intents_managment.run:main
+      """
+      )
